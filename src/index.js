ad0b41867373cb45d5eb4a43f2b4abcefbca638a
shanmuga.priya <shanmuga.priya@SPs-MacBook-Pro.local>
2020-12-11 10:35:40 +0530

    react test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import registerServiceWorker from  './registerServiceWorker';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
